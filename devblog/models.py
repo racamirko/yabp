1bb7176cf569342f7cc8eb95b035fd91f7dfa18e
Mirko Raca <racamirko@gmail.com>
2015-08-20 11:06:32 +0200

    4

--- devblog/models.py
+++ devblog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+class Story(models.Model):
+    title = models.CharField(max_length=300)
+    body = models.TextField()
+    date = models.TimeField(auto_now_add=True)
+    tag = models.ManyToManyField(Tag)
+
