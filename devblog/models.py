4c10b6ea424af9c883fdeb9326e0c92dd1614496
Mirko Raca <racamirko@gmail.com>
2015-08-20 11:41:14 +0200

    Lorem-ipsum population of model data

--- devblog/models.py
+++ devblog/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+import loremipsum as LI
+import random as rnd
 
 # Create your models here.
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "Tag: "+self.name
+
 
 class Story(models.Model):
     title = models.CharField(max_length=300)
@@ -11,3 +18,20 @@ class Story(models.Model):
     date = models.TimeField(auto_now_add=True)
     tag = models.ManyToManyField(Tag)
 
+    def __str__(self):
+        return "Story: "+self.title
+
+
+def auto_populate(num_of_stories=10, num_of_tags=10):
+    # generate tags
+    for i in xrange(num_of_tags):
+        t = Tag(name=LI.get_sentence().split()[0])
+        t.save()
+    tags = Tag.objects.all()
+    for i in xrange(num_of_stories):
+        s = Story(title=LI.get_sentence(), body=LI.get_paragraphs(4))
+        s.save()
+        for j in xrange(rnd.randint(1, 4)):
+            tag_idx = rnd.randint(0, len(tags)-1)
+            s.tag.add(tags[tag_idx])
+        s.save()
--- devblog/views.py
+++ devblog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from models import *
 
 # Create your views here.
 
@@ -7,4 +8,4 @@ def home(request):
     return render(request, 'home.html')
 
 def print_story(request, id):
-    pass
+    # return render(request, 'home.html')
