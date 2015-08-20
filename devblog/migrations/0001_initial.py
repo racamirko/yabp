6db89046714f31dc6a4a462a82c34a874dda0ffc
Mirko Raca <racamirko@gmail.com>
2015-08-20 14:28:23 +0200

    Missing files

---
+++ devblog/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Story',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=300)),
+                ('body', models.TextField()),
+                ('date', models.TimeField(auto_now_add=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='story',
+            name='tag',
+            field=models.ManyToManyField(to='devblog.Tag'),
+        ),
+    ]
